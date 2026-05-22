ebb31c01ca74c087ffe98e786b2637c84161eda0
Thomas Alvenin <105067427+ThomasAlvenin@users.noreply.github.com>
2026-05-22 20:59:19 +0200

    Kodio: update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 
-#!/bin/bash
+#!/bin/basha
 HOME="/home/container"
 HOMEA="$HOME/linux/.apt"
 STAR1="$HOMEA/lib:$HOMEA/usr/lib:$HOMEA/var/lib:$HOMEA/usr/lib/x86_64-linux-gnu:$HOMEA/lib/x86_64-linux-gnu:$HOMEA/lib:$HOMEA/usr/lib/sudo"
